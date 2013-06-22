328aef1f64bed756fd321d74cedce2ade5528e0b
MineForeman <neil@mineforeman.com>
2013-06-22 23:29:32 +0100

    Checkin for release Beta 0.2.0 (2013-7-23)

--- head.php
+++ head.php
@@ -6,8 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-        <meta http-equiv="refresh" content="600">
-
+	<meta http-equiv="refresh" content="600">
+	<!-- Le javascript ================================================== -->
+    <script type="text/javascript" src="js/bootstrap-transition.js"></script>
+    <script type="text/javascript" src="js/bootstrap-alert.js"></script>
+    <script type="text/javascript" src="js/bootstrap-modal.js"></script>
+    <script type="text/javascript" src="js/bootstrap-dropdown.js"></script>
+    <script type="text/javascript" src="js/bootstrap-scrollspy.js"></script>
+    <script type="text/javascript" src="js/bootstrap-tab.js"></script>
+    <script type="text/javascript" src="js/bootstrap-tooltip.js"></script>
+    <script type="text/javascript" src="js/bootstrap-popover.js"></script>
+    <script type="text/javascript" src="js/bootstrap-button.js"></script>
+    <script type="text/javascript" src="js/bootstrap-collapse.js"></script>
+    <script type="text/javascript" src="js/bootstrap-carousel.js"></script>
+    <script type="text/javascript" src="js/bootstrap-typeahead.js"></script>
+	<script type="text/javascript" src="js/jquery-latest.js"></script> 
+	<script type="text/javascript" src="js/jquery.tablesorter.js"></script>
+	<script type="text/javascript" id="js">
+	$(document).ready(function() {
+		//if (jQuery) {
+		//	alert("jquery loadedd");
+		//}      <link rel="stylesheet" href="js/themes/blue/style.css" type="text/css" id="" media="print, projection, screen" />
+		// call the tablesorter plugin
+		$("#stats").tablesorter();
+	}); </script>
+	<!-- ================================================== -->
     <!-- Le styles -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <style>
@@ -16,7 +39,8 @@
       }
     </style>
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="css/bootstrap-minepeon.css" rel="stylesheet">
+
+	<link href="css/bootstrap-minepeon.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -26,9 +50,9 @@
     <!-- Fav and touch icons
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-        <link rel="shortcut icon" href="ico/favicon.png">-->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="ico/favicon.png">-->
   </head>
 
   <body>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 include('ssl.inc.php');
 include('timezone.inc.php');
+require('miner.inc.php');
 
 create_graph("mhsav-hour.png", "-1h", "Hourly");
 create_graph("mhsav-day.png", "-1d", "Daily");
@@ -28,19 +29,77 @@ function create_graph($output, $start, $title) {
   }
 }
 
+$stats = cgminer("devs");
+$status = $stats['STATUS'];
+$devs = $stats['DEVS'];
+
+
+
 include('head.php');
 include('menu.php');
 ?>
-        <div class="container">
-                <h1>Graphs</h1>
-                <div class=graph><img src="rrd/mhsav-hour.png" alt="mhsav.png" /></div>
-                <div class=graph><img src="rrd/mhsav-day.png" alt="mhsav.png" /><img src="rrd/mhsav-week.png" alt="mhsav.png" /></div>
-                <div class=graph><img src="rrd/mhsav-month.png" alt="mhsav.png" /><img src="rrd/mhsav-year.png" alt="mhsav.png" /></div>
-        </div>
 
 	<div>
 		<center>Update Time: <?php echo date('D, d M Y H:i:s T') ?><center>
 	</div>
+	<div class="container">
+		<h1>Graphs</h1>
+			<div class=graph><img src="rrd/mhsav-hour.png" alt="mhsav.png" /></div>
+			<div class=graph><img src="rrd/mhsav-day.png" alt="mhsav.png" /><img src="rrd/mhsav-week.png" alt="mhsav.png" /></div>
+		<div class=graph><img src="rrd/mhsav-month.png" alt="mhsav.png" /><img src="rrd/mhsav-year.png" alt="mhsav.png" /></div>
+	</div>
+
+
+	<div class="container">
+		<table id="stats" class="tablesorter table table-striped table-bordered table-hover table-condensed stats">
+			<thead> 
+			<tr>
+				<th>Name</th>
+				<th>ID</th>
+				<th>Temp</th>
+				<th>MH/s</th>
+				<th>Accept</th>
+				<th>Reject</th>
+				<th>Error</th>
+				<th>Utility</th>
+				<th>Last Share Time</th>
+			</tr>
+			</thead>
+			<tbody> 
+			<?php echo statsTable($devs);  ?>
+			</tbody> 
+		</table>
+
+	</div>
 <?php
 
 include('foot.php');
+
+function statsTable($devs) {
+
+	$tableRow = "";
+
+	foreach ($devs as $dev) {
+		
+		if ($dev['MHS5s'] > 0) {
+		$tableRow = $tableRow . "
+			<tr>
+
+				<td>" . $dev['Name'] . "</td>
+				<td>" . $dev['ID'] . "</td>
+				<td>" . $dev['Temperature'] . "</td>
+				<td>" . $dev['MHSav'] . "</td>
+				<td>" . $dev['Accepted'] . "</td>
+				<td>" . $dev['Rejected'] . "</td>
+				<td>" . $dev['HardwareErrors'] . "</td>
+				<td>" . $dev['Utility'] . "</td>
+				<td>" . date('h:i:s', $dev['LastShareTime']) . "</td>
+			</tr>";
+			
+		
+		}
+	}
+
+	return $tableRow;
+
+}
