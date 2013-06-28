01c43be41fcef23b4842672f5d8369e2843a78ab
MineForeman <neil@mineforeman.com>
2013-06-28 05:36:51 +0100

    Status update

--- inc/settings.inc.php
+++ inc/settings.inc.php
@@ -1,20 +1,5 @@
 <?php
 
-$file = "/opt/minepeon/etc/minepeon.conf";
 
-// read the miner config file
-
-// $settings = json_decode(file_get_contents($file), true);
-
-// decode the json
-//$data = json_decode($minerConf, true);
-
-/*
-$settings = array(
-                "timezone" => "timezone",
-                "other" => "other",
-)
-*/	
-// Recode into JSON and save
-file_put_contents($file, json_encode($settings));
+$donation = file_get_contents("/opt/minepeon/etc/donation");
 
