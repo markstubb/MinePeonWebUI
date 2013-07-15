4633c323628440d9e9aae723807f46d97c42265b
joeykrim <joeykrim@gmail.com>
2013-07-15 19:44:59 -0400

    Removed unnecessary </td> tag

--- index.php
+++ index.php
@@ -248,7 +248,7 @@ function poolsTable($pools) {
 				<td>" . $pool['Discarded'] . "</td>
 				<td>" . date('H:i:s', $pool['LastShareTime']) . "</td>				
 				<td>" . $pool['Diff1Shares'] . "</td>				
-				<td>" . $pool['DifficultyAccepted'] . " ["  . round(($pool['DifficultyAccepted'] / $pool['Diff1Shares']) * 100, 2) .  "%]</td></td>
+				<td>" . $pool['DifficultyAccepted'] . " ["  . round(($pool['DifficultyAccepted'] / $pool['Diff1Shares']) * 100, 2) .  "%]</td>
 				<td>" . $pool['DifficultyRejected'] . " ["  . round(($pool['DifficultyRejected'] / $pool['Diff1Shares']) * 100, 2) .  "%]</td>
 				<td>" . $pool['LastShareDifficulty'] . "</td>
 				<td>" . $pool['BestShare'] . "</td>			
