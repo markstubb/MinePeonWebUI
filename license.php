95213839c550da9264b53d3249b5c6e2fe2eee19
MineForeman <neil@mineforeman.com>
2013-07-15 06:52:34 +0100

    Another typo in license, thanks Phil

--- license.php
+++ license.php
@@ -12,7 +12,7 @@ include('menu.php');
         <div class="container">
                 <h1>License</h1> 
 				<div>
-				<p>There are many different components to MinePeon and you will find that they all have some sort of license, please respect their intellectual property rights by compiling with their license, you will find the licence details included in the upstream package of the software.</p>
+				<p>There are many different components to MinePeon and you will find that they all have some sort of license, please respect their intellectual property rights by complying with their license, you will find the licence details included in the upstream package of the software.</p>
 
 				<p>The MinePeon software is Copyright 2013 Neil Fincham under the GPLv3.</p>
 <pre>
