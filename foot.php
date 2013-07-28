f7221e0863fb6734a9f4123291552ae983e39f39
thgh <thoogh@gmail.com>
2013-07-29 00:43:52 +0200

    Cleanup styles, fixed tablesorter, update to bootstrap3RC1

--- foot.php
+++ foot.php
@@ -1,5 +1,16 @@
+<footer class="container">
+  <hr />
+  Server Time: <?php echo date('D, d M Y H:i:s T') ?>
+</footer>
 
- 
+<script type="text/javascript" src="js/jquery.min.js"></script> 
+<script type="text/javascript" src="js/jquery.tablesorter.js"></script>
+<script type="text/javascript" id="js">
+  $(document).ready(function() {
+    $(".tablesorter").tablesorter();
+    console.log("Table sorted");
+  });
+</script>
 
-  </body>
+</body>
 </html>
