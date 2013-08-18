46e866bb9c084fde9a66ad71fa3f0e8afc597bc4
MineForeman <neil@mineforeman.com>
2013-08-18 23:53:41 +0100

    Kill the miner on restart

--- inc/miner.inc.php
+++ inc/miner.inc.php
@@ -3,27 +3,27 @@
 
 function cgminer($command, $parameter) {
 
-	$command = array (
-		"command"  => $command,
-		"parameter" => $parameter
-	);
-	
-	$jsonCmd = json_encode($command);
+        $command = array (
+                "command"  => $command,
+                "parameter" => $parameter
+        );
 
-	$host = "127.0.0.1";
-	$port = 4028;
+        $jsonCmd = json_encode($command);
 
-	$client = @stream_socket_client("tcp://$host:$port", $errno, $errorMessage);
+        $host = "127.0.0.1";
+        $port = 4028;
 
-	if ($client === false) {
-		return false;
-	}
-	fwrite($client, $jsonCmd);
-	$response = stream_get_contents($client);
-	fclose($client);
-	$response = preg_replace("/[^[:alnum:][:punct:]]/","",$response);
-	$response = json_decode($response, true);
-	return $response;
+        $client = @stream_socket_client("tcp://$host:$port", $errno, $errorMessage);
+
+        if ($client === false) {
+                return false;
+        }
+        fwrite($client, $jsonCmd);
+        $response = stream_get_contents($client);
+        fclose($client);
+        $response = preg_replace("/[^[:alnum:][:punct:]]/","",$response);
+        $response = json_decode($response, true);
+        return $response;
 
 }
 
